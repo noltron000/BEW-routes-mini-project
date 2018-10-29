0cb1bccbf8baccf79b0151a0f47213c3388704c9
Nolan Kovacik <noltron000@gmail.com>
2018-10-29 14:56:01 -0700

    auto reformat

--- app.js
+++ app.js
@@ -3,39 +3,38 @@ const app = express();
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({
-    extended: false
+	extended: false
 }))
 
 
 app.use(bodyParser.json())
 
-// use postman to test routes 
+// use postman to test routes
 
-// our models 
+// our models
 const venues = [{
-    name: 'AT&T Park',
-    events: []
+	name: 'AT&T Park',
+	events: []
 
 }, {
-    name: 'Dolores Park',
-    events: []
-
+	name: 'Dolores Park',
+	events: []
 }]
 
 
 app.get('/venues', (req, res) => {
-    // returns events in json
-    res.json(venues)
+	// returns events in json
+	res.json(venues)
 })
 
 
 app.post('/venues/events', (req, res) => {
-    // allow user to submit events
-    venues.push(req.body.events);
-    console.log(venues);
+	// allow user to submit events
+	venues.push(req.body.events);
+	console.log(venues);
 })
 
 
 app.listen(3000, () => {
-    console.log('working');
+	console.log('working');
 })
