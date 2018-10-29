d68689c88385ea03c862532bb28ee6d8988fa8ec
Nolan Kovacik <noltron000@gmail.com>
2018-10-29 14:56:24 -0700

    reformatting

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }))
 
-
 app.use(bodyParser.json())
 
 // use postman to test routes
-
 // our models
 const venues = [{
 	name: 'AT&T Park',
@@ -21,19 +19,16 @@ const venues = [{
 	events: []
 }]
 
-
 app.get('/venues', (req, res) => {
 	// returns events in json
 	res.json(venues)
 })
-
 
 app.post('/venues/events', (req, res) => {
 	// allow user to submit events
 	venues.push(req.body.events);
 	console.log(venues);
 })
-
 
 app.listen(3000, () => {
 	console.log('working');
